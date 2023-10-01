712f7fbd8243872fb9e328acc84fbba0817134ab
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-10-01 15:13:33 +0530

    assignment-1

--- data_manager.py
+++ data_manager.py
@@ -5,10 +5,11 @@ class DataManager:
     def load_data(self, csv_file):
         try:
             with open(csv_file, "r") as file:
-                header = file.readline()
+                header = file.readline().strip().split(",")
+                self.data.append(header)
                 for line in file:
                     self.data.append(line.strip().split(","))
-            print(f"Data has been loaded from {csv_file}")
+            print(f"Data loaded from {csv_file}")
         except FileNotFoundError:
             print(f"File '{csv_file}' not found.")
         except Exception as e:
@@ -18,15 +19,14 @@ class DataManager:
         if not self.data:
             print("No data to display.")
             return
-
         header = self.data[0]
         data = self.data[1:]
         max_lengths = [len(max(col, key=len)) for col in zip(*self.data)]
 
-        print(" | ".join(f"{header[i]:<{max_lengths[i]}}" for i in range(len(header))))
+        print("|".join(f"{header[i]:<{max_lengths[i]}}" for i in range(len(header))))
         print("-" * sum(max_lengths))
         for row in data:
-            print(" | ".join(f"{row[i]:<{max_lengths[i]}}" for i in range(len(row))))
+            print("|".join(f"{row[i]:<{max_lengths[i]}}" for i in range(len(row))))
 
     def sort_data(self, column_name):
         if not self.data:
@@ -49,46 +49,38 @@ class DataManager:
         if not self.data:
             print("No data to filter.")
             return
-
         header = self.data[0]
         data = self.data[1:]
-
         if column_name not in header:
             print(f"Column '{column_name}' not found.")
             return
-
         column_index = header.index(column_name)
         filtered_data = [header]
         for row in data:
             if row[column_index] == condition:
                 filtered_data.append(row)
-
         self.data = filtered_data
         print(f"Data filtered by '{column_name}' with condition '{condition}'")
 
     def getData(self):
-        if not self.data:
-            print("No data available.")
-            return None
         return self.data
 
 
-# Testing the class and its functions
+if __name__ == "__main__":
+    # Create a DataManager instance
+    df = DataManager()
 
-# Creating a DataManager instance
-data_manager = DataManager()
+    # Load data from a CSV file
+    df.load_data("./dataset.csv")
 
-# Loading data from a CSV file
-data_manager.load_data("./dataset.csv")
+    # Display data in a formatted table
+    df.display_data()
 
-# Displaying data in a formatted table
-data_manager.display_data()
+    # Filter data based on a condition
+    df.filter_data("SoldFlag", "1")
 
-# Sorting data based on a column name
-data_manager.sort_data("StrengthFactor")
+    # Sort data based on a column name
+    df.sort_data("StrengthFactor")
 
-# Filtering data based on a condition
-data_manager.filter_data("SoldFlag", "1")
-
-# Displaying the filtered data
-data_manager.display_data()
+    # Display the filtered data
+    df.display_data()
--- save_and_load_data.py
+++ save_and_load_data.py
@@ -1,26 +1,32 @@
 # In your main script
 from data_manager import DataManager
 import json
-import time as t
 
 # Create a DataManager instance and perform data operations (loading, filtering, sorting)
 df = DataManager()
 # Loading data from a CSV file
 df.load_data("./dataset.csv")
-t.sleep(2000)
-# Sorting data based on a column name
-df.sort_data("StrengthFactor")
-t.sleep(2000)
 # Filtering data based on a condition
 df.filter_data("SoldFlag", "1")
-
+# Sorting data based on a column name
+df.sort_data("StrengthFactor")
 data = df.getData()
 
 
 # Save the data to a JSON file
 def save_data_to_json(data, json_file):
+    header = data[0]
+    data = data[1:]
+    json_data = []
+    for row in data:
+        row_dict = {}
+        for i, value in enumerate(row):
+            header_name = header[i]
+            row_dict[header_name] = value
+        json_data.append(row_dict)
+
     with open(json_file, "w") as file:
-        json.dump(data, file, indent=4)
+        json.dump(json_data, file, indent=4)
 
 
 save_data_to_json(data, "filtered_and_sorted_data.json")
